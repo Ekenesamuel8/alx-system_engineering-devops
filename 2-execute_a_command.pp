f72662b9f0096964c5376b0af3230167374bd66a
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-04-28 13:32:38 -0700

    connecting ssh

--- 2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-#Using Puppet, create a manifest that kills a process named killmenow.
-exec { 'kill_killmenow_process':
-  command     => '/usr/bin/pkill killmenow',
-  refreshonly => true,
-  onlyif      => '/usr/bin/pgrep killmenow',
-}
-
